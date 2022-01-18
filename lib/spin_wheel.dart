f217bd9f808fea192e006b4a2b946ce0eea9d028
ashusharma7 <ashu7073657271@gmail.com>
2022-01-18 11:37:21 +0530

    Add glassmorphism UI

--- lib/spin_wheel.dart
+++ lib/spin_wheel.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:spin_wheel/fortune_wheel/core/core.dart';
 import 'package:spin_wheel/fortune_wheel/wheel/wheel.dart';
 
@@ -16,6 +18,24 @@ class SpinTheWheel extends StatefulWidget {
 class _SpinTheWheelState extends State<SpinTheWheel> {
   StreamController<int> controller = StreamController<int>();
 
+  List<String> win = [];
+  List<String> prize = [
+    "Rs. 100",
+    "Better luck\nnext time",
+    "Rs. 200",
+    "Rs.300",
+    "Rs. 400",
+    "Rs. 500"
+  ];
+  List<Color?> colors = [
+    Colors.yellow[700],
+    Colors.green[300],
+    Colors.blue.shade600,
+    Colors.red.shade400,
+    Colors.indigo[400],
+    Colors.green
+  ];
+
   @override
   void dispose() {
     controller.close();
@@ -25,96 +45,142 @@ class _SpinTheWheelState extends State<SpinTheWheel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
+      // backgroundColor: Colors.white,
+      body: Stack(
         children: [
           Container(
-            height: 400,
-            padding: const EdgeInsets.only(top: 50.0),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                FortuneWheel(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/bg1.jpg"),
+                    fit: BoxFit.fitHeight)),
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: Container(
+              color: Colors.white12,
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                children: [
+                  Container(
+                    height: 400,
+                    padding: const EdgeInsets.only(top: 50.0),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        FortuneWheel(
+                            onAnimationEnd: () {
+                              setState(() {});
+                            },
+                            // curve: Curves.bounceInOut,
+                            selected: controller.stream,
+                            items: prize
+                                .map((e) => FortuneItem(
+                                      style: const FortuneItemStyle(
+                                          color: Colors.white30,
+                                          borderColor: Colors.white24,
+                                          borderWidth: 1),
+                                      child: Text(
+                                        e,
+                                        style: TextStyle(
+                                            fontSize: 18.0,
+                                            fontWeight: win.last == e
+                                                ? FontWeight.bold
+                                                : FontWeight.normal,
+                                            color: win.last == e
+                                                ? Colors.white
+                                                : Colors.white70),
+                                      ),
+                                    ))
+                                .toList()),
+                        GestureDetector(
+                          onTap: () {
+                            int a = Random().nextInt(6);
 
-                    // curve: Curves.bounceInOut,
-                    selected: controller.stream,
-                    items: [
-                      FortuneItem(
-                        style: FortuneItemStyle(
-                            color: Colors.yellow[700]!, borderWidth: 0),
-                        child: const Text(
-                          "RS. 100",
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                      ),
-                      FortuneItem(
-                          style: FortuneItemStyle(
-                              color: Colors.green[300]!, borderWidth: 0),
-                          child: const Text(
-                            "Better Luck\nnext time",
-                            style: TextStyle(fontSize: 20.0),
-                          )),
-                      FortuneItem(
-                        child: const Text(
-                          "Rs. 200",
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                        style: FortuneItemStyle(
-                            color: Colors.blue.shade600, borderWidth: 0),
-                      ),
-                      FortuneItem(
-                        child: const Text(
-                          "Rs. 300",
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                        style: FortuneItemStyle(
-                            color: Colors.red.shade400, borderWidth: 0),
-                      ),
-                      FortuneItem(
-                        child: const Text(
-                          "Rs. 400",
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                        style: FortuneItemStyle(
-                            color: Colors.indigo[400]!, borderWidth: 0),
-                      ),
-                      const FortuneItem(
-                        child: Text(
-                          "Rs. 500",
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                        style: FortuneItemStyle(
-                            color: Colors.green, borderWidth: 0),
-                      ),
-                    ]),
-                GestureDetector(
-                  onTap: () {
-                    int a = Random().nextInt(6);
-                    controller.add(a);
-                  },
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Transform.rotate(
-                        angle: pi * 0.5,
-                        child: const Icon(
-                          EvaIcons.droplet,
-                          size: 100.0,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(right: 4.0),
-                        child: Text(
-                          "Spin",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      )
-                    ],
+                            win.add(prize[a]);
+
+                            controller.add(a);
+                          },
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Transform.rotate(
+                                angle: pi * 0.5,
+                                child: const Icon(
+                                  EvaIcons.droplet,
+                                  size: 100.0,
+                                  color: Colors.white54,
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(right: 4.0),
+                                child: Text(
+                                  "Spin",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      // color: Colors.black54,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                )
-              ],
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Win History",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white60,
+                            fontWeight: FontWeight.bold),
+                      )),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Expanded(
+                    child: Container(
+                      // height: MediaQuery.of(context).size.height / 2,
+                      // width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.white30),
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: win.length,
+                          reverse: true,
+                          itemBuilder: (context, index) {
+                            return Container(
+                                margin: const EdgeInsets.all(10.0),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      "${index + 1}",
+                                      style: const TextStyle(
+                                          fontSize: 25.0,
+                                          color: Colors.white70),
+                                    ),
+                                    const Spacer(),
+                                    Text(
+                                      win[index],
+                                      style: const TextStyle(
+                                          fontSize: 20.0,
+                                          color: Colors.white70),
+                                    ),
+                                  ],
+                                ));
+                          }),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  )
+                ],
+              ),
             ),
           ),
         ],
